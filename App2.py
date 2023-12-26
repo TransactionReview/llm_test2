03a9d726aefdd0b509540ee8b1db468a51c47b6e
TransactionReview <154547671+TransactionReview@users.noreply.github.com>
2023-12-27 00:28:10 +0900

    Add files via upload

---
+++ App2.py
@@ -0,0 +1,150 @@
+import streamlit as st
+import tiktoken # 텍스트 => chunk
+from loguru import logger # action 이력 로그
+
+from langchain.chains import ConversationalRetrievalChain # 메모리를 가지고 있는 체인
+from langchain.chat_models import ChatOpenAI # LLM모델
+
+# 여러 유형의 문서를 읽어 들일 수 있는 로더
+from langchain.document_loaders import PyPDFLoader 
+from langchain.document_loaders import Docx2txtLoader
+from langchain.document_loaders import UnstructuredPowerPointLoader
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter # 텍스트 나누기
+from langchain.embeddings import HuggingFaceEmbeddings # 한국어 특화된 임베딩 모델
+
+from langchain.memory import ConversationBufferMemory # 대화를 기억하는 메모리
+from langchain.vectorstores import FAISS # 벡터 저장(임시)
+
+from langchain.callbacks import get_openai_callback # 메모리 구현
+from langchain.memory import StreamlitChatMessageHistory # 메모리 구현
+
+def main():
+    st.set_page_config(
+        page_title="RAG TEST",
+        page_icon="🕶"
+    )
+    
+    st.title("_Private DAta :red[QA Chat]_ :books:")
+    
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+        
+    if "processComplete" not in st.session_state:
+        st.session_state.processComplete = None
+    
+    with st.sidebar:
+        uploaded_files = st.file_uploader("Upload your file",type=['pdf,docx','pptx'],accept_multiple_files=True)
+        openai_api_key = st.text_input("OpenAI API Key",key="chatbot_api_key",type="password")
+        process = st.button("Process")
+    
+    if process:
+        if not openai_api_key:
+            st.info(":fire: Please add your API Key to continue")
+            st.stop()
+        files_text = get_text(uploaded_files)
+        text_chunks = get_text_chunks(files_text)
+        vectorstore = get_vectorstore(text_chunks)
+        
+        st.session_state.conversation = get_conversation_chain(vectorstore, openai_api_key)
+        st.session_state.processComplete = True
+        
+    if 'messages' not in st.session_state:
+        st.session_state['messages'] = [{"role": "assistant", 
+                                        "content" : "안녕하세요! 주어진 문서에 대해 궁금하신 것이 있으면 언제든 물어봐주세요!:service_dog:"}]
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+            
+    history = StreamlitChatMessageHistory(key="chat_messages")
+    
+    if query := st.chat_input("질문을 입력해주세요."):
+        st.session_state.messages.append({"role": "user", "content": query})
+        # st.session_state.messages.append({"role": "assistant", "content": response})
+        
+        with st.chat_message("user"):
+            st.markdown(query)
+        
+        with st.chat_message("assistant"):
+            chain = st.session_state.conversation
+            
+            with st.spinner("Thinking..."):
+                result = chain({"question": query})
+                with get_openai_callback() as cb:
+                    st.session_state.chat_history = result['chat_history']
+                response = result['answer']
+                source_documents = result['source_documents']
+                
+                st.markdown(response)
+                with st.expander("참고 문서 확인"):
+                    st.markdown(source_documents[0].metadata['source'],help=source_documents[0].page_content)
+                    st.markdown(source_documents[1].metadata['source'],help=source_documents[1].page_content)
+                    st.markdown(source_documents[2].metadata['source'],help=source_documents[2].page_content)
+        
+        st.session_state.messages.append({"role": "assistant", "content":response})
+        
+def tiktoken_len(text):
+    tokenizer = tiktoken.get_encoding("cl100k_base")
+    tokens = tokenizer.encode(text)
+    return len(tokens)
+
+def get_text(docs):
+    
+    doc_list = []
+    
+    for doc in docs:
+        file_name = doc.name
+        with open(file_name, "wb") as file:
+            file.write(doc.getvalue())
+            logger.info(f"Uploaded {file_name}")
+        if '.pdf' in doc.name:
+            loader = PyPDFLoader(file_name)
+            documents = loader.load_and_split()
+        elif '.docx' in doc.name:
+            loader = Docx2txtLoader(file_name)
+            documents = loader.load_and_split()
+        elif '.pptx' in doc.name:
+            loader = UnstructuredPowerPointLoader(file_name)
+            documents = loader.load_and_split()
+        
+        doc_list.extend(documents)
+    return doc_list
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 900,
+        chunk_overlap=100,
+        length_function = tiktoken_len
+    )
+    chunks = text_splitter.split_documents(text)
+    return chunks
+
+def get_vectorstore(text_chunks):
+    embeddings = HuggingFaceEmbeddings(
+                                        model_name = "jhgan/ko-sroberta-multitask", # ★★★★★
+                                        model_kwargs = {'device':'cpu'},
+                                        encode_kwargs = {'normalize_embeddings': True}
+                                        )
+    vectordb = FAISS.from_documents(text_chunks, embeddings)
+    return vectordb
+# https://huggingface.co/jhgan/ko-sroberta-multitask
+
+def get_conversation_chain(vectorstore, openai_api_key):
+    llm = ChatOpenAI(openai_api_key=openai_api_key, model_name='gpt-3.5-turbo',temperature=0)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+            llm=llm,
+            chain_type = "stuff",
+            retriever=vectorstore.as_retriever(search_type = 'mmr', vervose = True),
+            memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True, output_key='answer'),
+            get_chat_history = lambda h:h, # 메모리가 들어온 그대로 chat_history에 넣음
+            return_source_documents = True,
+            verbose = True
+        )
+    return conversation_chain
+
+if __name__ == '__main__':
+    main()
